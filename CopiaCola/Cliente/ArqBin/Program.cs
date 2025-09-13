4319cf61eb27ceccb4f2c6ec9f2b4ba64729e2d0
unisanta <unisanta@santacecilia.br>
2025-09-12 21:26:12 -0300

    feat: TP Copia e cola

---
+++ CopiaCola/Cliente/ArqBin/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Net.Sockets;
+using System.Net;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FileStream infile;
+            int tam;
+            char x;
+
+            infile = new System.IO.FileStream("arquivo.txt", System.IO.FileMode.Open, System.IO.FileAccess.Read);
+
+
+            tam = (int)infile.Length;
+            char[] arquivos = new char[tam];
+            for (int i = 0; i < tam; ++i)
+            {
+                x = (char)infile.ReadByte();
+                //outfile.WriteByte((byte)x);
+                arquivos[i] = x;
+            }
+
+            enviarArquivo(arquivos);
+            infile.Close();
+        }
+
+        static void enviarArquivo(char[] arquivo)
+        {
+            Socket socketenviar = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.IP);
+            IPEndPoint endereco = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 9060);
+
+            Console.ReadKey();
+            socketenviar.SendTo(Encoding.ASCII.GetBytes(arquivo), endereco);
+            socketenviar.Close();
+        }
+    }
+}
---
+++ CopiaCola/ServidorRecebe/ServidorRecebe/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+using System.IO;
+
+namespace ServidorRecebe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Socket socketreceber = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.IP);
+            EndPoint endereco = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 9060);
+            byte[] data = new byte[1024];
+            int  qtdbytes;
+
+            socketreceber.Bind(endereco);
+           
+            qtdbytes = socketreceber.ReceiveFrom(data, ref endereco);
+            string recebido = Encoding.ASCII.GetString(data, 0, qtdbytes);
+            Console.WriteLine(recebido);
+
+            SalvarArquivo(recebido);
+        }
+
+        static void SalvarArquivo(String recebido)
+        {
+            FileStream outfile = new System.IO.FileStream("arquivo_escrito.txt", System.IO.FileMode.Create, System.IO.FileAccess.Write);
+            for (int i = 0; i < recebido.Length; ++i)
+            {
+                outfile.WriteByte((byte)recebido[i]);
+            }
+            outfile.Close();
+        }
+    }
+}
